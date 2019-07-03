df1def0bb8f1ee7c3804d99650b60c1800f51b01
kagawagao <kingsongao1221@gmail.com>
2019-07-03 10:26:52 +0800

    feat: update page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
-import React from 'react';
-import logo from './assets/logo.png';
-import './App.css';
+import React from "react";
+import logo from "./assets/logo.png";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        Make data visible, readable and programmable for programmers
-        </p>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  state = {
+    y: 0
+  };
+  componentDidMount() {
+    this.timer = setInterval(() => {
+      this.setState(({ y }) => ({
+        y: y === -96 ? 0 : y - 48
+      }));
+    }, 1500);
+  }
+  componentWillUnmount() {
+    if (this.timer) {
+      clearInterval(this.timer);
+      this.timer = null;
+    }
+  }
+  render() {
+    const { y } = this.state;
+    return (
+      <div className="app">
+        <header className="app-header">
+          <img src={logo} className="app-logo" alt="logo" />
+          <div>
+            <span>Make Data</span>
+            <div className="app-features">
+              <div
+                className="app-features-inner"
+                style={{
+                  transform: `translate3d(0, ${y}px, 0)`
+                }}
+              >
+                <span>Visible</span>
+                <span>Readable</span>
+                <span>Programmable</span>
+              </div>
+            </div>
+            <span>For Programmers</span>
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
-
-export default App;
